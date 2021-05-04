ac2fefd9473766b571ac394b4357c4a5675b106b
Venkat Venkataraju <venkat.venkataraju@gmail.com>
2021-05-03 21:06:31 -0700

    updated test code to cycle thru LEDs and colors

--- example_code/src/main.cpp
+++ example_code/src/main.cpp
@@ -2,37 +2,65 @@
 #include <FastLED.h>
 
 #if TARGET_CHIPSET == ATTINY85
-  #define CLOCK_PIN 2
-  #define DATA_PIN 0
+#define CLOCK_PIN 2
+#define DATA_PIN 0
 #elif TARGET_CHIPSET == ESP8266
-  #define CLOCK_PIN 14
-  #define DATA_PIN 13
+#define CLOCK_PIN 14
+#define DATA_PIN 13
 #endif
+#define NUM_LEDS 10
 
-#define NUM_LEDS 9
+#define DELAY 42
 
 CRGB leds[NUM_LEDS];
 
-void set_color(CRGB color) {
-  for(int i=0; i<NUM_LEDS; i++) {
-    leds[i] = color;
+int8_t pos;
+int8_t dir;
+
+uint16_t hue = 0;
+uint8_t delta = 1;
+
+bool do_update;
+uint32_t last_updated;
+
+void setup()
+{
+  FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
+  pos = 0;
+  dir = 1;
+}
+
+void update_color()
+{
+  hue += delta;
+  hue = hue % 360;
+
+  fill_solid(leds, NUM_LEDS, CRGB::Black); // set all LEDs to black
+  leds[pos] = CHSV(hue, 255, 255);         // set the current pos to computed color (saturation=255, value=255)
+  blur1d(leds, NUM_LEDS, 32);              // speared color to 2 nearest neighbor
+  FastLED.show();
+
+  pos = pos + dir;
+  if ((pos < 0) || (pos >= NUM_LEDS))
+  {
+    dir = dir * -1;
+    pos = (pos < 0) ? 0 : pos;
+    pos = (pos >= NUM_LEDS) ? NUM_LEDS - 1 : pos;
   }
 }
 
-void setup() {
-  FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
-}
+void loop()
+{
+  if (do_update)
+  {
+    update_color();
 
-void loop() {
-  set_color(CRGB::Red);
-  FastLED.show();
-  delay(500);
+    last_updated = millis();
+    do_update = false;
+  }
 
-  set_color(CRGB::Green);
-  FastLED.show();
-  delay(500);
-
-  set_color(CRGB::Blue);
-  FastLED.show();
-  delay(500);
+  if (millis() - last_updated > DELAY)
+  {
+    do_update = true;
+  }
 }
